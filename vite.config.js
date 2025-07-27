27c7e013e0301469d8d5cd0ea52871ed05403cd6
StickMouille <59177908+StickMouille@users.noreply.github.com>
2025-07-28 01:18:31 +0200

    added tailwind + shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
+import path from "path"
+import { fileURLToPath } from 'url'
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      'react-dom/client': 'react-dom'
+    }
+  },
 })
